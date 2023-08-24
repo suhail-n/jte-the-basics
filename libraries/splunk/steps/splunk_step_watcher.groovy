c703ff5c7e27c34c0acc364174030f466d7c3892
suhail-n <suhail.nooristani@gmail.com>
2023-08-23 22:58:40 -0400

    added splunk before and after hooks

---
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -0,0 +1,9 @@
+@BeforeStep // call before each step
+void before() {
+    println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step" 
+}
+
+@AfterStep
+void after() {
+    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step" 
+}
