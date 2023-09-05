10c058b7ae56adc94c3620adc118c8ef49a909b1
suhail-n <suhail.nooristani@gmail.com>
2023-09-05 13:36:27 -0400

    added a utilities

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -1,4 +1,8 @@
+import utilities.utilities
 void call() {
+    stage("Greeting") {
+        utilities.echoGreeting()
+    }
     stage("Gradle: Build") {
         println "build from the gradle library"
     }
